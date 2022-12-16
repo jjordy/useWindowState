bdbfed5e25b5042e0a0efc00e557e56d4b36c0c7
Jordan Addison <jordanrileyaddison@gmail.com>
2022-12-16 16:07:12 -0600

    Adds optional qs overrides as options argument to hook.

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,5 @@
 import useWindowState from ".";
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act, cleanup } from "@testing-library/react-hooks";
 
 it("Should insert the initial values into the url", async () => {
   expect(window.location.search).toBe("");
@@ -20,6 +20,7 @@ it("Should insert the initial values while preserving any existing values", () =
 });
 
 it("Should update a value into the url", () => {
+  window.history.pushState({ path: "/" }, "", "/");
   const valueToTest = {
     test: {
       test: {
@@ -29,8 +30,6 @@ it("Should update a value into the url", () => {
       },
     },
   };
-
-  window.history.pushState({ path: "/?test=test" }, "", "/?test=test");
   const { result } = renderHook(() =>
     useWindowState({ initialState: { limit: 0, offset: 0 } })
   );
@@ -42,7 +41,16 @@ it("Should update a value into the url", () => {
   expect(result.current.state).toEqual({
     limit: 0,
     offset: 0,
-    test: "test",
     newValue: valueToTest,
   });
+});
+
+it("Pass in custom qs options thru the options argument overriding the defaults.", () => {
+  const { result } = renderHook(() =>
+    useWindowState({
+      initialState: { limit: 0, offset: 0 },
+      options: { decoder: () => `` },
+    })
+  );
+  expect(result.current.state).toEqual({});
 });
